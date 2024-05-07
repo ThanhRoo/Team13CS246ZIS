e899999babfa476b28f92cff69a1f1295a3fe97b
vthanh2811 <139070405+suss1128@users.noreply.github.com>
2024-05-07 22:24:43 +0700

    test
    vthanh

--- VoleTanThanh_1259/Project_Airline_Ticket_Booking_System/src/DataBase/JDBCUtil.java
+++ VoleTanThanh_1259/Project_Airline_Ticket_Booking_System/src/DataBase/JDBCUtil.java
@@ -1,38 +1,37 @@
-
 package DataBase;
+
 import java.sql.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-
-
 
 /**
  *
  * @author Legion 5 Pro
  */
 public class JDBCUtil {
-    public static Connection getConnection(){
+
+    public static Connection getConnection() {
         Connection c = null;
         try {
             DriverManager.registerDriver(new com.mysql.jdbc.Driver());
-            
+
             String url = "jdbc:mySQL://localhost:3306/quanlidatve";
             String user = "root";
             String password = "";
-            
-            c = DriverManager.getConnection(url,user,password);
+
+            c = DriverManager.getConnection(url, user, password);
         } catch (SQLException e) {
             e.printStackTrace();
         }
-       
-        return c; 
+
+        return c;
     }
-    
-    public static void closeConnection(Connection c){
+
+    public static void closeConnection(Connection c) {
         try {
-            if(c != null){
-            c.close();
-        }
+            if (c != null) {
+                c.close();
+            }
         } catch (Exception e) {
         }
     }
